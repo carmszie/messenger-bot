b868e076119bfb4b1419164c68264184c338f627
carmszie <carmela.icban@adamson.edu.ph>
2026-02-27 13:17:16 +0800

    try2

--- app.py
+++ app.py
@@ -278,4 +278,4 @@ def handle_message():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
